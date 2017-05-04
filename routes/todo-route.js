3327697f122e7b5968359532fa5a7a61cab8dae7
Suresh Selvaraj <Suresh Selvaraj>
2017-05-03 21:21:04 -0600

    adding more get operations

--- routes/todo-route.js
+++ routes/todo-route.js
@@ -45,4 +45,65 @@ Todo.find().then( (data)=>{
 
 })
 
+
+router.get('/getTodos/:cat/:todoTxt',(req,res)=>{
+
+console.log(' i m in getTodo router' , req.params);
+console.log(' i m in getTodo router' , req.params);
+console.log(' i m in getTodo router' , req.params.todoTxt);
+
+//res.send(req.params);
+
+if(req.params.cat==='text')
+{
+        Todo.find({'text':req.params.todoTxt } ).then( (data)=>{
+            console.log('list of todos ',data);
+                res.status(200).send({
+                        data
+                });
+        },(err)=>{
+                console.log('error in fetching todo ',err);
+                res.status(400).send(err);
+            }).catch((err)=>{
+                console.log('error in connecting todo db',err);
+                res.status(404).send(err);
+            })
+
+}else if(req.params.cat==='status')
+{
+    if ( ( req.params.todoTxt==='true') || ( req.params.todoTxt==='false') )
+    {
+                Todo.find({'iscompleted':req.params.todoTxt } ).then( (data)=>{
+                         console.log('list of todos ',data);
+                        
+                        console.log('data length', data.length);
+
+                        if(data.length>0)
+                        {
+                             res.status(200).send({
+                                data
+                            });
+                        }else{
+                            res.status(200).send('Result not found');
+                        }
+
+                        
+        },(err)=>{
+                console.log('error in fetching todo ',err);
+                res.status(400).send(err);
+            }).catch((err)=>{
+                console.log('error in connecting todo db',err);
+                res.status(404).send(err);
+            })
+    } else{
+        res.status(400).send('Invalid status. it should be either true or false');
+    }
+
+}
+
+
+
+})
+
+
 module.exports=router;
